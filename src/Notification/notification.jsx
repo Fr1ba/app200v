fd64d7acd35403c1839ee81741632029d4501473
Fr1ba <laub99@gmail.com>
2025-03-13 14:31:18 +0100

    You can now click on pepper to add a notis, and click on notis to remove

--- src/Notification/notification.jsx
+++ src/Notification/notification.jsx
@@ -1,16 +1,33 @@
-import styles from './notification.module.css'
-import pepper from './img/pepper.png'
+import {useState} from 'react';
+import styles from './notification.module.css';
+import pepper from './img/pepper.png';
+
 
 function Notification () {
-    const notis = ["New message", "Updated status", "Case closed"];
+   const [notis, setNotis] = useState([]);
+   const randomNotisList = ["Sunny day", "Rainy day", "Snowy day", "Darkies day"];
+
+   const handleClick = () => {
+    // choose a random notis from randomNotisList
+    const randomIndex = Math.floor(Math.random()* randomNotisList.length);
+    const newNotis = randomNotisList[randomIndex];
+
+    // Update notis-array with new notis
+    setNotis(prevNotis => [...prevNotis, newNotis]);
+   };
+
+   // remove li item when clicked
+   const removeNotis = (indexToRemove) => {
+    setNotis(prevNotis => prevNotis.filter((_, index) => index !== indexToRemove));
+   };
 
     return (
         <>
         <h1>Notifications</h1>
-            <img src={pepper} alt ="pepper" />
+            <img src={pepper} alt ="pepper" onClick={handleClick} />
         <ul>
             {notis.map((noti, index) => (
-            <li key={index}>Case {index}: {noti}</li>))
+            <li key={index} onClick={()=> removeNotis(index)}>Case {index}: {noti}</li>))
             }
         </ul>
         </>
