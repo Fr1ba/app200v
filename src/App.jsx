7e71c451afc417db4eb2a9d9585067ee61c901f7
vennyy2 <vendelasoiland@hotmail.com>
2025-03-20 12:02:20 +0100

    Tried to fix css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import Login from './Login/Login.jsx/';
+import Login from "./Login/Login.jsx/";
 import Header from "./Header/header.jsx";
-import CaseList from './case list/CaseList.jsx';
-import CreateCase from './CreateCase/CreateCase.jsx';
+import CaseList from "./case list/CaseList.jsx";
+import CreateCase from "./CreateCase/CreateCase.jsx";
 import ProfilePage from "./ProfilePage/ProfilePage";
-import NotificationBell from './Notification/NotificationBell.jsx';
+import NotificationBell from "./Notification/NotificationBell.jsx";
 
-function App(){
-    return( 
-      <>
-      </
-    );
+function App() {
+  return (
+    <>
+      <ProfilePage />
+    </>
+  );
 }
 
 export default App;
