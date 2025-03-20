aa222cb772b8e6c723345044a99db18e4c7e96fd
NikolaDeja <nikolaa.deja@gmail.com>
2025-03-20 11:13:51 +0100

    update

--- src/case list/Case.jsx
+++ src/case list/Case.jsx
@@ -3,7 +3,7 @@ function Case({caseTitle, caseCategory, caseStatus}) {
 
     return (
         <div className={styles.case}>
-            <div className={styles.circle} style={caseStatus ? {backgroundColor: "blue"} : {backgroundColor: "red"}}></div>
+            <div className={styles.circle} style={caseStatus ? {backgroundColor: "lightgreen"} : {backgroundColor: "lightgray"}}></div>
             <h1 className={styles.text}>{caseTitle}</h1>
             <p className={styles.text}>{caseCategory}</p>
         </div>
