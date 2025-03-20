a8b9c60da265d044e75897ddcb1a8dfb5b17e408
Fr1ba <laub99@gmail.com>
2025-03-20 11:11:06 +0100

    added TODO comments

--- src/Notification/NotificationBell.jsx
+++ src/Notification/NotificationBell.jsx
@@ -1,3 +1,8 @@
+// TODO Make the bell fully colored in when the notifications is open. 
+// TODO Add a number on the bell with how many unread notifications is there. If more than five display "5+".
+// TODO When you click on the notif you will be directed to the related case and the notif will be gone from the bell. 
+
+
 import { useState } from "react";
 import { Bell } from "lucide-react";
 import styles from './bell.module.css';
