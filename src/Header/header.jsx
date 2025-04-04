0353a497884dc71e77c49e43cdcc92e92526921b
Fr1ba <laub99@gmail.com>
2025-04-04 11:52:50 +0200

    header responsive

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,23 +1,37 @@
 import styles from './header.module.css';
-import React from 'react';
+import React, { useState } from 'react';
 import NotificationBell from './Notification/NotificationBell';
-import ProfileIcon from './ProfileIcon/profileIcon'
+import ProfileIcon from './ProfileIcon/profileIcon';
 import logo from '../images/logo.png';
 import { Link } from 'react-router-dom';
+import { Menu, X } from 'lucide-react'; // Importerer ikoner for hamburgermeny
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className={styles.header}>
       <div className={styles.headerLeft}>
         <Link to="/CaseList" className={styles.headerLogo}>
-          <img src= { logo } alt="Logo" className={styles.logo}></img>
+          <img src={logo} alt="Logo" className={styles.logo}></img>
         </Link>
-        <ul className={styles.navLinks}>
-          <li><Link to="/CaseList">Hjem</Link></li>
-          <li><Link to="/CreateCase">Opprett ny Sak</Link></li>
+        
+        {/* Hamburger-ikon som vises på mobile enheter */}
+        <button className={styles.menuButton} onClick={toggleMenu}>
+          {menuOpen ? <X /> : <Menu />}
+        </button>
+        
+        {/* Navigasjonslenker som endrer visning basert på skjermstørrelse */}
+        <ul className={`${styles.navLinks} ${menuOpen ? styles.navLinksActive : ''}`}>
+          <li><Link to="/CaseList" onClick={toggleMenu}>Hjem</Link></li>
+          <li><Link to="/CreateCase" onClick={toggleMenu}>Opprett ny Sak</Link></li>
         </ul>
       </div>
-      
+     
       <div className={styles.headerRight}>
         <NotificationBell/>
         <ProfileIcon/>
