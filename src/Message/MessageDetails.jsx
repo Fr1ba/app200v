5107e4b6d3480fc6a3e70fe472b284943f56bbb9
vennyy2 <vendelasoiland@hotmail.com>
2025-05-21 13:11:39 +0200

    fiksa detaljer

--- src/Message/MessageDetails.jsx
+++ src/Message/MessageDetails.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import styles from "./MessageDetails.module.css";
 
 function MessageDetails() {
   const [caseDetails, setCaseDetails] = useState(null);
