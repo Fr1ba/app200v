77b545fefc53e09af0d3d7309c020d3926e9972c
Fr1ba <laub99@gmail.com>
2025-05-28 10:30:54 +0200

    HEader changes

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -6,48 +6,52 @@ import logo from '../images/logo.png';
 import { Link } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
 
-// Mobileview of header
+const endpoint = "https://app06.itxnorge.no";
+
+async function Logout() {
+  await fetch(`${endpoint}/rest/core/logout`, {
+    method: 'POST',
+    credentials: 'include'
+  });
+}
+
 function MobileHeader() {
   const [menuOpen, setMenuOpen] = useState(false);
- 
+
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-  // Close the menu when the user clicks on a link
   const closeMenu = () => {
     setMenuOpen(false);
   };
 
   return (
-    <nav className={styles.mobileHeader}>
-      <div className={styles.mobileHeaderTop}>
+    <nav className={`${styles.header} ${styles.mobileHeader}`}>
+      <div className={styles.headerContainer}>
         <Link to="/" className={styles.headerLogo}>
           <img src={logo} alt="Logo" className={styles.logo} />
         </Link>
-        
+       
         <div className={styles.headerRight}>
           <NotificationBell />
-          <button 
-            className={styles.menuButton} 
-            onClick={toggleMenu} 
+          <button
+            className={styles.menuButton}
+            onClick={toggleMenu}
             aria-label="Åpne meny"
           >
             {menuOpen ? <X /> : <Menu />}
           </button>
         </div>
-        
-
       </div>
-      
-      {/* Navigation for mobileview*/}
+     
       {menuOpen && (
         <div className={styles.mobileNav}>
-          <ul className={styles.mobileNavLinks}>
+          <ul className={`${styles.navLinks} ${styles.mobileNavLinks}`}>
             <li><Link to="/" onClick={closeMenu}>Hjem</Link></li>
             <li><Link to="/CreateCase" onClick={closeMenu}>Opprett ny Sak</Link></li>
-            <li ><Link to="/ProfilePage">Profil</Link></li>
-            <li onClick={Logout}><Link to="/Login">Logg ut</Link></li>
+            <li><Link to="/ProfilePage" onClick={closeMenu}>Profil</Link></li>
+            <li onClick={Logout}><Link to="/Login" onClick={closeMenu}>Logg ut</Link></li>
           </ul>
         </div>
       )}
@@ -55,30 +59,30 @@ function MobileHeader() {
   );
 }
 
-// Desktopview of header
 function DesktopHeader() {
   return (
-    <nav className={styles.desktopHeader}>
-      <div className={styles.headerLeft}>
-        <Link to="/" className={styles.headerLogo}>
-          <img src={logo} alt="Logo" className={styles.logo} />
-        </Link>
-        
-        <ul className={styles.desktopNavLinks}>
-          <li><Link to="/">Hjem</Link></li>
-          <li><Link to="/CreateCase">Opprett ny Sak</Link></li>
-        </ul>
-      </div>
-      
-      <div className={styles.headerRight}>
-        <NotificationBell />
-        <ProfileIcon />
+    <nav className={`${styles.header} ${styles.desktopHeader}`}>
+      <div className={styles.headerContainer}>
+        <div className={styles.headerLeft}>
+          <Link to="/" className={styles.headerLogo}>
+            <img src={logo} alt="Logo" className={styles.logo} />
+          </Link>
+         
+          <ul className={`${styles.navLinks} ${styles.desktopNavLinks}`}>
+            <li><Link to="/">Hjem</Link></li>
+            <li><Link to="/CreateCase">Opprett ny Sak</Link></li>
+          </ul>
+        </div>
+       
+        <div className={styles.headerRight}>
+          <NotificationBell />
+          <ProfileIcon />
+        </div>
       </div>
     </nav>
   );
 }
 
-// Main component that chooses the right header based on screen size
 function Header() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
@@ -94,16 +98,6 @@ function Header() {
   }, []);
 
   return isMobile ? <MobileHeader /> : <DesktopHeader />;
-}
-
-//Function copied from profileIcon.jsx to be able to logout from mobileview
-const endpoint = "https://app06.itxnorge.no"
-
-async function Logout() {
-    await fetch(`${endpoint}/rest/core/logout`, {
-        method: 'POST',
-        credentials: 'include'
-    });
 }
 
 export default Header;
